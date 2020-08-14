f1aecba8ac2764f344d1faf89424d28b98195021
Jakub Piskorz <kontakt@jakubpiskorz.pl>
2020-08-14 07:44:13 +0200

    hotfix

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { useStaticQuery, graphql } from "gatsby";
 
-const Footer = () => {
+const Footer = (props) => {
   const data = useStaticQuery(graphql`
     query {
       site {
