5426022903eecc3376214b50d6a4233a9dc14e77
Jakub Piskorz <kontakt@jakubpiskorz.pl>
2020-08-21 06:23:08 +0200

    hotfix

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,29 +1,17 @@
 import React from "react"
 import Footer from "./footer"
 import Header from "./header"
-import styled from "styled-components"
 import SEO from "./seo"
+import LayoutStyle from "./layout.module.scss"
 
 const Layout = props => {
-  const Container = styled.div`
-    display: flex;
-    flex-direction: column;
-    margin: auto;
-    max-width: 1300px;
-    padding: 40px;
-    min-height: 100vh;
-  `
-  const InnerContainer = styled.div`
-    flex-grow: 1;
-  `
-
   return (
-    <Container>
+    <div className={LayoutStyle.container}>
       <SEO title={props.title} author={props.author} />
       <Header title={props.title}></Header>
-      <InnerContainer>{props.children}</InnerContainer>
+      <div className={LayoutStyle.innerContainer}>{props.children}</div>
       <Footer author={props.author}></Footer>
-    </Container>
+    </div>
   )
 }
 
